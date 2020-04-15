b5d0a2df9dee8a4b90a95addae357d2630837c09
Isaac Oliveira <61355645+Isaac0liveira@users.noreply.github.com>
2020-04-15 07:55:28 -0300

    Average with pairs using Python.

--- Python/averagePairs.py
+++ Python/averagePairs.py
@@ -7,7 +7,7 @@ while(loop > -1):
        break
     elif valor1 == -1:
        break
-    if valor1 and valor2 % 2 == 0:
+    if valor1 % 2 == 0 and valor2 % 2 == 0:
        print('A média desses valores é ', media)
     else:
         print('Apenas valores pares!!!')
