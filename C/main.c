3cd2b464ec508752d371d3df97585ac2387c5aca
Isaac Oliveira <61355645+Isaac0liveira@users.noreply.github.com>
2020-09-01 13:55:04 -0300

    Sudoku

--- C/main.c
+++ C/main.c
@@ -40,7 +40,7 @@ int verify(int indexI, int indexJ, int lista[9][9]){
     for (int i = 0; i < 9; ++i) {
         if(index != indexI){
             if(lista[indexI][indexJ] == lista[i][indexJ]){
-                wprintf(L"Numero na posição %d da coluna %d repetido \n", indexJ, index);
+                wprintf(L"Numero na posição de coluna %d da linha %d repetido \n", indexJ, index);
                 return 1;
             }
         }
@@ -50,63 +50,27 @@ int verify(int indexI, int indexJ, int lista[9][9]){
     for (int i = 0; i < 9; ++i) {
         if(index2 != indexJ){
             if(lista[indexI][indexJ] == lista[indexI][i]){
-                wprintf(L" Numero na posição %d da coluna %d repetido! \n", index2, indexI);
+                wprintf(L" Numero na posição de coluna %d na linha %d repetido! \n", index2, indexI);
                 return 1;
             }
         }
         index2 += 1;
     }
     char posicao[40];
-    if((indexI >= 0 && indexI <= 2) && (indexJ >=0 && indexJ <=2)){
-        index = 0;
-        index2 = 0;
-        strcpy(posicao, "Numero repetido na matriz 0 da coluna 0");
-    }else if((indexI >= 3 && indexI <= 5) && (indexJ >=0 && indexJ <=2)){
-        index = 3;
-        index2 = 0;
-        strcpy(posicao, "Numero repetido na matriz 1 da coluna 0");
-    }else if((indexI >= 6 && indexI <= 8) && (indexJ >=0 && indexJ <=2)){
-        index = 6;
-        index2 = 0;
-        strcpy(posicao, "Numero repetido na matriz 2 da coluna 0");
-    }else if((indexI >= 0 && indexI <= 2) && (indexJ >=3 && indexJ <=5)){
-        index = 0;
-        index2 = 3;
-        strcpy(posicao, "Numero repetido na matriz 0 da coluna 1");
-    }else if((indexI >= 3 && indexI <= 5) && (indexJ >=3 && indexJ <=5)){
-        index = 3;
-        index2 = 3;
-        strcpy(posicao, "Numero repetido na matriz 1 da coluna 1");
-    }else if((indexI >= 6 && indexI <= 8) && (indexJ >=6 && indexJ <=8)){
-        index = 6;
-        index2 = 3;
-        strcpy(posicao, "Numero repetido na matriz 2 da coluna 1");
-    }else if((indexI >= 0 && indexI <= 2) && (indexJ >=6 && indexJ <=8)){
-        index = 0;
-        index2 = 6;
-        strcpy(posicao,"Numero repetido na matriz 0 da coluna 2");
-    }else if((indexI >= 3 && indexI <= 5) && (indexJ >=6 && indexJ <=8)){
-        index = 3;
-        index2 = 6;
-        strcpy(posicao, "Numero repetido na matriz 1 da coluna 2");
-    }else if((indexI >= 6 && indexI <= 8) && (indexJ >=6 && indexJ <=8)){
-        index = 5;
-        index2 = 6;
-        strcpy(posicao, "Numero repetido na matriz 2 da coluna 2");
-    }
+    index = 0, index2 = 0;
+    index = (indexI / 3) * 3;
+    index2 = (indexJ / 3) * 3;
     int guardIndex = index2;
     for (int i = 0; i < 3; ++i) {
         index2 = guardIndex;
         for (int j = 0; j < 3; ++j) {
-            if(index != indexI && indexJ != index2){
-                if (lista[indexI][indexJ] == lista[index][index2]){
-                    printf("%s! \n", posicao);
+            if((index + i) != indexI && (index + j) != indexJ) {
+                if (lista[index + i][index2 + j] == lista[indexI][indexJ]) {
+                    wprintf(L"Número repetido na matriz da coluna %d na linha %d \n", index2, index);
                     return 1;
                 }
             }
-            index2 += 1;
         }
-        index += 1;
     }
     printf("Nenhum\n");
     return 0;
@@ -114,12 +78,13 @@ int verify(int indexI, int indexJ, int lista[9][9]){
 
 void write(int lista[9][9]){
     setlocale(LC_ALL, "portuguese");
+    int guardavalor;
     int indexI = 0;
     for (int i = 0; i < 9; ++i) {
         int indexJ = 0;
         for (int j = 0; j < 9; ++j) {
             if(lista[i][j] == 0){
-                wprintf(L"Digite um valor entre 1 à 10 para a posição %d da coluna %d: \n", indexJ, indexI);
+                wprintf(L"Digite um valor entre 1 à 10 para a posição %d da linha %d: \n", indexJ, indexI);
                 scanf("%d", &lista[i][j]);
                 while((lista[i][j] >= 10 && lista [i][j] <= 0) || verify(indexI, indexJ, lista) == 1 ){
                     wprintf(L"Erro! Digite um numero entre 1 à 10: \n");
@@ -131,64 +96,21 @@ void write(int lista[9][9]){
         }
         indexI += 1;
     }
-    printf("Fim de jogo, você conseguiu!");
+    wprintf(L"Fim de jogo, você conseguiu!");
 }
 
  void main() {
-     int lista[9][9];
+     int lista[9][9] = {{0,0,1,0,0,2,4,0,6},
+                        {6,0,0,0,0,5,0,1,8},
+                        {0,8,2,9,1,6,0,0,0},
+                        {2,0,0,8,0,4,5,0,0},
+                        {1,0,8,0,0,0,6,0,9},
+                        {0,0,0,0,0,0,8,2,7},
+                        {5,1,4,7,3,0,0,6,2},
+                        {3,0,7,2,4,0,0,0,0},
+                        {0,2,0,0,0,1,3,7,0}};
 
-     for (int i = 0; i < 9; ++i) {
-         for (int j = 0; j < 9; ++j) {
-             lista[i][j] = 0;
-         }
-     }
 
-    lista[1][0] = 6;
-    lista[4][0] = 1;
-    lista[6][0] = 5;
-    lista[7][0] = 3;
-
-    lista[2][1] = 8;
-    lista[6][1] = 1;
-    lista[8][1] = 2;
-
-    lista[0][2] = 1;
-    lista[2][2] = 2;
-    lista[4][2] = 8;
-    lista[6][2] = 4;
-    lista[7][2] = 7;
-
-    lista[2][3] = 9;
-    lista[3][3] = 8;
-    lista[6][3] = 7;
-    lista[7][3] = 2;
-
-    lista[2][4] = 1;
-    lista[6][4] = 3;
-    lista[7][4] = 4;
-
-    lista[0][5] = 2;
-    lista[1][5] = 5;
-    lista[2][5] = 6;
-    lista[3][5] = 4;
-    lista[8][5] = 1;
-
-    lista[0][6] = 4;
-    lista[3][6] = 5;
-    lista[4][6] = 6;
-    lista[5][6] = 8;
-    lista[8][6] = 3;
-
-    lista[1][7] = 1;
-    lista[5][7] = 2;
-    lista[6][7] = 6;
-    lista[8][7] = 7;
-
-    lista[0][8] = 6;
-    lista[1][8] = 8;
-    lista[4][8] = 9;
-    lista[5][8] = 7;
-    lista[6][8] = 2;
     print(lista);
     write(lista);
  }
